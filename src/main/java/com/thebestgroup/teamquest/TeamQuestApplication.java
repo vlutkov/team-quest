124646b44d1a42c7b9000661c5f8d0a80b4b6229
Uladzimir Lutkou <lutkov.vladimir@otr.ru>
2023-02-03 15:43:26 +0300

    #3 Создать проект

---
+++ src/main/java/com/thebestgroup/teamquest/TeamQuestApplication.java
@@ -0,0 +1,13 @@
+package com.thebestgroup.teamquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thebestgroup/teamquest/TeamQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thebestgroup.teamquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
